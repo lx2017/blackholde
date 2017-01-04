bc561488f8935dedb5057bef50fe9dce90001f4d
lx2017 <18296157209@sina.cn>
2017-01-04 19:01:50 +0800

    xiufu mysql connection

--- blackholde/Application/Library/Db/Mysql.php
+++ blackholde/Application/Library/Db/Mysql.php
@@ -3,14 +3,13 @@ class Mysql
 {
 	private $config = array();
   private static $Driver_Object = NULL;
-    
+  //查询语句或者执行语句的条件表达式
+  protected $options  =   array();
+  // 数据库表名称
+  protected $model = NULL;
 	public function __construct($config)
 	{
         spl_autoload_register('Mysql::autoload');  
-
-
-
-      
         if(self::$Driver_Object === NULL){
             self::$Driver_Object = new Driver($config);
         }
@@ -18,37 +17,92 @@ class Mysql
 	}
   /*
    执行MYSQL 操作的表名
+   @param data 数据名称
   */
   public function data($data = NULL)
   {
-
+     if($this->model==NULL)
+     {
+         $this->model = $data;
+     }
   }
     /**
-    读取数据库的条件语句处理
+    读取数据库的条件语句处理,这里仿THINKPHP3.2
+    @param where:条件语句
+    @param parse:预处理语句
     */
-  public function where($where)
+  public function where($where,$parse=NULL)
 	{
-      echo "<pre>";
-      print_r($where);
-      echo "</pre>";
-      return $this; 
+      if(is_string($where) && !is_null($parse))
+      {
+         
+          if(is_string($parse) && !is_null($parse))
+          {
+               $parse = array_shift(func_get_args());
+          }
+          $parse = array_map(array($this,'escapeString'),$parse);
+          $where =   vsprintf($where,$parse);
+      }
+      if(is_string($where) && '' != $where){
+          $map    =   array();
+          $map['_string']   =   $where;
+          $where  =   $map;
+        } 
+     if(isset($this->options['where']))
+     {
+         $this->options['where'] =  array_merge($this->options['where'],$where);
+     }else{
+         $this->options['where'] = $where;
+     }
+     return $this;
 	}
-	/*
-     执行语句
-    */
-    public function query($sql)
+	 
+  /*
+    SQL 连接语句的拼接
+    @param:join 为字符串
+    @param:type 连接类型
+    说明:这里的JOIN参数只支持字符串类型
+  */
+    public function join($join,$type="INNER")
     {
-    
+      try
+      {
+
+         if(!is_string($join))
+         {
+             throw new Exception("JOIN 操作只支持字符串");
+             die();
+         }
+         $join = trim($join," ");//对字符串左右的空格进行过滤
+         //对JOIN SQL 语句进行简单的拼接
+         $join    =   (false !== stripos($join,'JOIN'))? $join :$type.' JOIN '.$join;
+         if(!isset($this->options['join']))
+         {
+             $this->options['join'] = array();
+         }
+
+         $this->options['join'][] = $join;
+        
+         return $this;
+         
+      }catch(Exception $e)
+      {
+        echo $e->getMessage();
+        die();
+      }
     }
+
    /*
     条件参数的过滤
    */
-    public function where_filter($where)
+    public function where_filter()
     {
-
+            echo "<pre>";
+            print_r($this->options['join']);
+            echo "</pre>";
     }
     public function escapeString($str) {
-        return str_ireplace("'", "''", $str);
+        return addslashes(str_ireplace("'", "''", $str));
     }
    
      /*
@@ -75,5 +129,9 @@ class Mysql
 
           }
      }
+     /*
+       MYSQL SQL 参数过滤
+     */
+    
 }
 ?>
--- blackholde/Home/Action/Member.php
+++ blackholde/Home/Action/Member.php
@@ -8,11 +8,10 @@ class Member extends Base
 		require_once(HOLDE_ROUTE."DB.php");
 		//$url = "mysql://username:passwd@localhost:3306/DbName?param1=val1&param2=val2#utf8";
 		$data=DB::getConfig();
-		$where = array(
-          "name"=>"jack",
-          "age"=>20
+		$where =  array(
+               'name'=>'jack'
 			);
-	   $data->where($where);
+	   $data->where($where)->join("JOIN acd name on a.id=b.id")->join("JOIN jkjk on a.id=b.id")->where_filter();
 	}
 }
 ?>
